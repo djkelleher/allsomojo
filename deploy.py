188d4ed166528a3a103112fd91b45987ba19ca95
Dan Kelleher <kelleherjdan@gmail.com>
2024-08-27 09:16:08 -0400

    update for dependencies

--- deploy.py
+++ deploy.py
@@ -1,18 +1,19 @@
 """Deploy as systemd services. Run `taskflows create deploy.py`."""
-from taskflows.service import Calendar, Service, mamba_command
+
+from taskflows.service import Calendar, MambaEnv, Service
+
+venv = MambaEnv("allsomojo")
 
 default_update = Service(
     name="allsomojo-update-default",
-    command=mamba_command(
-        env_name="allsomojo", command="allsomojo update-db update-sheet"
-    ),
-    schedule=Calendar("Mon..Sat 1:00 America/New_York"),
+    venv=venv,
+    start_command="allsomojo update-db update-sheet",
+    start_schedule=Calendar("Mon..Sat 1:00 America/New_York"),
 )
 
 update_recheck_blacklist = Service(
     name="allsomojo-update-recheck-blacklist",
-    command=mamba_command(
-        env_name="allsomojo", command="allsomojo update-db -b update-sheet"
-    ),
-    schedule=Calendar("Sun 1:00 America/New_York"),
+    venv=venv,
+    start_command="allsomojo update-db -b update-sheet",
+    start_schedule=Calendar("Sun 1:00 America/New_York"),
 )
